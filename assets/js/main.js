bef836c6dbc6bb9e8172f6853d5f04f1959d3e06
asadbek <asadbekqulboyev@gmail.com>
2025-10-21 14:02:39 +0500

    hero finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,13 @@ $(".header__hamburger").on("click", function () {
   $(this).toggleClass("active");
   $(".header__menu").toggleClass("active");
 });
+// steps
+$(".steps_header").on("click", function () {
+  const parent = $(this).closest(".steps_item");
+  if (parent.hasClass("active")) {
+    parent.removeClass("active");
+  } else {
+    $(".steps_item").removeClass("active");
+    parent.addClass("active");
+  }
+});
